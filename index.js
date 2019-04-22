ec0e495a384d4055b4a6af087bff813eec7c38b0
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-04-22 23:03:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name){
+  var message = `Welcome, ${name}. You are number ${current.length + 1} in line.`;
+  return message;
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    currentCustomer = katzDeliLine[0];
+    katzDeliLine.slice(1);
+    return currentCustomer;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length > 0) {
+    var currentLineString = "The line is currently: "
+      for (i = 0; i < katzDeliLine.length; i++){
+        currentLineString = currentLineString + `${i}. ${katzDeliLine[i]}, `
+      }
+      return currentLineString;
+    } else {
+      return "The line is currently empty."
+    }
+  
+}
